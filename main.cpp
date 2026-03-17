ee6aa00c3eb6c9a7e6744a04b0a12737f15af104
mul <mulcoding@gmail.com>
2026-03-17 08:14:54 +0700

    chapter 8.8 question #2

--- main.cpp
+++ main.cpp
@@ -1,51 +1,14 @@
 #include <iostream>
 
-int calculate(int x, int y, char op);
-int getInteger();
-char getOperator();
-
 int main() {
-  int x{getInteger()};
-  int y{getInteger()};
-  char op{getOperator()};
+  std::cout << "ASCII code  | letter\n";
 
-  int result{calculate(x, y, op)};
+  char letter{'a'};
+  while (letter <= 'z') {
+    std::cout << static_cast<int>(letter) << "  | " << letter << "\n";
 
-  std::cout << x << ' ' << op << ' ' << y << " = " << result << "\n";
+    ++letter;
+  }
 
   return 0;
-}
-
-int calculate(int x, int y, char op) {
-  switch (op) {
-  case '+':
-    return x + y;
-  case '-':
-    return x - y;
-  case '*':
-    return x * y;
-  case '/':
-    return x / y;
-  case '%':
-    return x % y;
-  default:
-    std::cout << "ERROR";
-    return 0;
-  }
-}
-
-int getInteger() {
-  std::cout << "Enter an integer: ";
-  int value{};
-  std::cin >> value;
-
-  return value;
-}
-
-char getOperator() {
-  std::cout << "Enter operator +, -, *, / or %: ";
-  char op{};
-  std::cin >> op;
-
-  return op;
 }
